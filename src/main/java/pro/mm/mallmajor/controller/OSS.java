de52fa3ec9cb5e430a1871769bc590efba70bf65
1zca0 <yizheng1991@hotmail.com>
2021-03-23 23:47:17 +0800

    add oss controller

---
+++ src/main/java/pro/mm/mallmajor/controller/OSS.java
@@ -0,0 +1,18 @@
+package pro.mm.mallmajor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: mallmono
+ * @Package: pro.mm.mallmajor.controller
+ * @ClassName: OSS
+ * @Author: Frank.Cao
+ * @Description:
+ * @Date: 2021/3/23 23:46
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping
+public class OSS {
+}
